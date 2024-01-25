391703d2c3e26dadc356d86bf35d7789033e005e
zn1p <a.tamulaitis@gmail.com>
2024-01-25 14:11:43 +0200

    Second

--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -30,7 +30,7 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     public override string Name => "Template";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("701CBCF1-6FFE-4B6B-8DD9-A8A7034E22A0");
 
     /// <summary>
     /// Gets the current plugin instance.
